2712f40889220026b55fe1f5c4b472e6310f9fc2
keechio <keechidotty123@yahoo.com>
2017-09-28 18:42:31 -0700

    fixed package location

--- 2017/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/servoTest.java
+++ 2017/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/servoTest.java
@@ -27,7 +27,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package org.firstinspires.ftc.robotcontroller.external.samples;
+package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -52,7 +52,6 @@ import com.qualcomm.robotcore.util.Range;
  */
 
 @TeleOp(name="servoTest", group="Linear Opmode")
-@Disabled
 public class servoTest extends LinearOpMode {
 
     // Declare OpMode members.
